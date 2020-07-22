5adcd7bd1b8fae5f00e1d401c155b2cfe819004f
Jayla Nickens <jaylanickensgill11@gmail.com>
2020-07-22 16:34:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+Fact1 = "Fact one is Koalas sleep for 18 hours a day."
+Fact2 = "Koalas are amazing swimmers and can swim across a river."
+Fact3 = "female koalas are mature by the age of two."
+Fact4 = "koalas are picky eater, they only eat three types of leaves."
+
+print (Fact1)
+print (Fact2)
+print (Fact3)
+print (Fact4)
